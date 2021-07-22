4aced65c9edc78ce977bd515c33dcfa2e0fba8bc
罗涛 <799204419@qq.com>
2021-07-22 11:55:08 +0800

    init

---
+++ packages/calendar/calendar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { createNamespace } from '../_utils';
+
+const { prefixedName: name } = createNamespace('actionSheet');
+
+export default defineComponent({
+  name,
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+@import './src/index.scss';
+</style>
